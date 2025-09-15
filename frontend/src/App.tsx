0c689daaf784a7be6d3a217b6c680c18a26d78ef
Abhishek Mishra <abhim5920@gmail.com>
2025-09-15 14:28:00 +0000

    make canvas full screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,8 @@ function App() {
   useEffect(() => {
     if (canvasRef.current) {
       const canvas = canvasRef.current
+      canvas.width  = window.innerWidth;
+      canvas.height = window.innerHeight;
       const ctx = canvasRef.current.getContext('2d')
 
       if (ctx) {
@@ -25,14 +27,14 @@ function App() {
 
   return (
     <>
-      <div className='h-screen w-screen bg-black text-white'>
+      <div className='h-screen w-screen bg-black text-white m-0 p-0'>
         <div className='absolute top-0 left-[40%] w-84 mx-auto bg-white text-black flex gap-5 justify-around'>
           <button className='hover:text-gray-500' onClick={()=>setShapeType('rect')}>rectangle</button>
           <button className='hover:text-gray-500' onClick={()=>setShapeType('circle')}>circle</button>
           <button className='hover:text-gray-500' onClick={()=>setShapeType('line')}>line</button>
           <button className='hover:text-gray-500' onClick={()=>setShapeType('pencil')}>pencil</button>
         </div>
-        <canvas ref={canvasRef} width={"1026"} height={"500"} className='border rounded' ></canvas>
+        <canvas ref={canvasRef}  className='border rounded block' ></canvas>
       </div>
     </>
   )

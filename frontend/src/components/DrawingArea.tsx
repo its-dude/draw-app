0d9847215f71fbbd9adae96ac472e9ca9406bcd4
Abhishek Mishra <abhim5920@gmail.com>
2025-09-25 17:11:28 +0000

    fix[ui]: adjust header pointer-events so canvas drawing works below toolbar

--- frontend/src/components/DrawingArea.tsx
+++ frontend/src/components/DrawingArea.tsx
@@ -11,21 +11,21 @@ export function DrawingArea({ setSelectedTool, setModalType, canvasRef, selected
     return (
     
     <div className='h-screen w-screen bg-black text-white m-0 p-0'>
-        <div className='header fixed w-full top-4 flex  bg-pink z-100'>
-            <div className='left-[40%] w-84 mx-auto bg-white text-black flex gap-5 justify-around '>
+        <div className='header fixed w-full top-4 flex  bg-pink z-100 pointer-events-none'>
+            <div className='left-[40%] w-84 mx-auto bg-white text-black flex gap-5 justify-around pointer-events-auto'>
                 <button className='hover:text-gray-500' onClick={() => setSelectedTool('rect')}>rectangle</button>
                 <button className='hover:text-gray-500' onClick={() => setSelectedTool('circle')}>circle</button>
                 <button className='hover:text-gray-500' onClick={() => setSelectedTool('line')}>line</button>
                 <button className='hover:text-gray-500' onClick={() => setSelectedTool('pencil')}>pencil</button>
             </div>
-            <div className='mr-4 flex gap-4'>
-                <button className='bg-purple-500 py-2 px-4 rounded-md ' onClick={() => {
+            <div className='mr-4 flex gap-4 pointer-events-none'>
+                <button className='bg-purple-500 py-2 px-4 rounded-md pointer-events-auto' onClick={() => {
                     // @ts-ignore
                     window.selected = selectedTool;
                     setSelectedTool("pencil");
                     setModalType('share_room')
                 }}>Share Room</button>
-                <button className='bg-blue-500 py-2 px-4 rounded-md' onClick={() => {
+                <button className='bg-blue-500 py-2 px-4 rounded-md pointer-events-auto' onClick={() => {
                     // @ts-ignore
                     window.selected = selectedTool;
                     setSelectedTool("pencil");
@@ -34,6 +34,6 @@ export function DrawingArea({ setSelectedTool, setModalType, canvasRef, selected
             </div>
         </div>
         <div></div>
-        <canvas ref={canvasRef} className='' ></canvas>
+        <canvas ref={canvasRef} className='absolute inset-0 w-[100%] h-[100%] ' ></canvas>
     </div>)
 }

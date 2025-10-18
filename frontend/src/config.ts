b9b35243bad365cee3c390e88ea355fb2835d099
Abhishek Mishra <abhim5920@gmail.com>
2025-10-18 14:27:22 +0530

    fix: change backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,2 @@
 export const getSecret = () => localStorage.getItem('token');
-export const Backend_URL = 'http://localhost:3000/api'
+export const Backend_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api";

a7fa01ad4add75050a6310fbbecc1cb6adf60ba3
Abhishek Mishra <abhim5920@gmail.com>
2025-10-18 19:06:44 +0530

    fix docker file

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,3 @@
 export const getSecret = () => localStorage.getItem('token');
-export const Backend_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:3000/api";
+export const Backend_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:3000/api";
+console.log(import.meta.env.VITE_BACKEND_URL,"--",process.env.VITE_BACKEND_URL)

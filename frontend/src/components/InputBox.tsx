356c01f22cb189ae7c27749ca42263294bbf602a
Abhishek Mishra <abhim5920@gmail.com>
2025-10-19 20:07:00 +0530

    add nginx config file for react routing

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -7,5 +7,5 @@ type InputType = {
 }
 
 export function InputBox({placeholder, type, onChange}: InputType){
-    return  <input onChange={onChange} className="border-0 shadow py-2 px-1 rounded outline-0" type={type} placeholder={placeholder} />
+    return  <input onChange={onChange} className="border-0 shadow py-2 px-1 rounded outline-0" type={type} placeholder={placeholder} autoComplete="off" />
 }
